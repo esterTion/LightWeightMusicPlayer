ac94fc7fecf16fe56c90d20e9fca18e46076c795
esterTion <lcz970@126.com>
2017-11-05 20:42:03 +0800

    Reformat & improve listener

--- musicplayer.js
+++ musicplayer.js
@@ -13,205 +13,212 @@
   	loop：循环
  *
  */
-MusicPlayer=function(config){
+window.MusicPlayer = function (config) {
 	'use strict';
-	if(!config.element || !config.src)
+	if (!config.element || !config.src)
 		return false;
-	this.element=config.element;
-	this.config=config;
-	if(document.getElementById(this.config.id||'')!=null){
+	this.element = config.element;
+	this.config = config;
+	if (document.getElementById(this.config.id || '') != null) {
 		console.warn('existing element id, ignoring and generating new id');
 		delete this.config.id;
 	}
-	var defaultConfig={
-		color:{
-			played:'#FFFFFF',
-			buffered:'#C8C7C7',
-			blank:'#918F8F',
-			border:'#000000'
+	var resource = {
+		play: '<svg style="width:18px;height:18px"><polygon style="fill:#333;stroke:#FFF" points="2,2 2,16 16,9"></polygon></svg>',
+		pause: '<svg style="width:18px;height:18px"><polygon style="fill:#333;stroke:#FFF" points="2,2 2,16 8,16 8,2"></polygon><polygon style="fill:#333;stroke:#FFF" points="10,2 10,16 16,16 16,2"></polygon></svg>'
+	};
+	var defaultConfig = {
+		color: {
+			played: '#FFFFFF',
+			buffered: '#C8C7C7',
+			blank: '#918F8F',
+			border: '#000000'
 		},
-		id:'MusicPlayer_'+Date.now(),
-		borderWidth:0,
-		autoPlay:false,
-		loop:false
+		id: 'MusicPlayer_' + Date.now(),
+		borderWidth: 0,
+		autoPlay: false,
+		loop: false
 	},
-	div=document.createElement('div'),k1,k2;
+		div = document.createElement('div'), k1, k2;
 	/* 一级默认参数 */
-	for(k1 of ['id','borderWidth','autoPlay','loop']){
-		this.config[k1]=this.config[k1]||defaultConfig[k1]
-	}
+	['id', 'borderWidth', 'autoPlay', 'loop'].forEach(function (k1) {
+		this.config[k1] = this.config[k1] || defaultConfig[k1];
+	});
 	/* 二级默认参数 */
-	for(k1 of ['color']){
-		if(this.config[k1]==undefined){
-			this.config[k1]=defaultConfig[k1];
-			break;
+	['color'].forEach(function (k1) {
+		if (this.config[k1] == undefined) {
+			this.config[k1] = defaultConfig[k1];
+			return;
 		}
-		for(k2 in defaultConfig[k1]){
-			this.config[k1][k2]=this.config[k1][k2]||defaultConfig[k1][k2];
+		for (k2 in defaultConfig[k1]) {
+			this.config[k1][k2] = this.config[k1][k2] || defaultConfig[k1][k2];
 		}
-	}
-	div.id=this.config.id;
-	if(typeof(this.config.borderWidth)!='string')
-		this.config.borderWidth=this.config.borderWidth+'px';
+	});
+	div.id = this.config.id;
+	if (typeof (this.config.borderWidth) != 'string')
+		this.config.borderWidth = this.config.borderWidth + 'px';
 	this.element.appendChild(div);
-	div.innerHTML='<div class="warpper"><div class="buffered"></div><div class="played"></div><div class="icon">'+this.resource.play+'</div>'+
-		'<style>'+
-			'#'+this.config.id+'{height:20px;width:100%;position:relative;background:'+this.config.color.border+';cursor:pointer;-moz-user-select:none;-webkit-user-select:none;-ms-user-select:none}'+
-			'#'+this.config.id+' .warpper{background:'+this.config.color.blank+';position:absolute;left:'+this.config.borderWidth+';right:'+this.config.borderWidth+';top:'+this.config.borderWidth+';bottom:'+this.config.borderWidth+'}'+
-			'#'+this.config.id+' .buffered{position:absolute;height:100%;background:'+this.config.color.buffered+';transition:width .5s}'+
-			'#'+this.config.id+' .played{position:absolute;height:100%;background:'+this.config.color.played+';transition:width .5s}'+
-			'#'+this.config.id+' .icon{height:18px;width:18px;margin:0 auto;opacity:0.5}'+
-		'</style>'+
-		'<audio src="'+this.config.src+'"></audio></div>';
-	this.container=document.getElementById(this.config.id);
-	this.barPlay=document.querySelector('#'+this.config.id+' .played');
-	this.barBuffer=document.querySelector('#'+this.config.id+' .buffered');
-	this.audio=document.querySelector('#'+this.config.id+' audio');
-	this.svgIcon=document.querySelector('#'+this.config.id+' .icon');
-	var self=this;
-	this.updateTime=function(p){
-		this.barPlay.style.width=p+'%';
-	}
-	this.dragging=!1;
-	this.dragged=!1;
-	this.listeners={
-		timeupdate:function(e){
-			if(!self.dragging)
-				self.updateTime(this.currentTime/this.duration*100);
-			try{
-				var s,e,i=0;
-				for(;i<this.buffered.length;i++){
-					if( this.currentTime>this.buffered.start(i) && this.currentTime<this.buffered.end(i) ){
-						s=this.buffered.start(i);
-						e=this.buffered.end(i);
+	div.innerHTML = '<div class="warpper"><div class="buffered"></div><div class="played"></div><div class="icon">' + resource.play + '</div>' +
+		'<style>' +
+		'#' + this.config.id + '{height:20px;width:100%;position:relative;background:' + this.config.color.border + ';cursor:pointer;-moz-user-select:none;-webkit-user-select:none;-ms-user-select:none}' +
+		'#' + this.config.id + ' .warpper{background:' + this.config.color.blank + ';position:absolute;left:' + this.config.borderWidth + ';right:' + this.config.borderWidth + ';top:' + this.config.borderWidth + ';bottom:' + this.config.borderWidth + '}' +
+		'#' + this.config.id + ' .buffered{position:absolute;height:100%;background:' + this.config.color.buffered + ';transition:width .5s}' +
+		'#' + this.config.id + ' .played{position:absolute;height:100%;background:' + this.config.color.played + ';transition:width .5s}' +
+		'#' + this.config.id + ' .icon{height:18px;width:18px;margin:0 auto;opacity:0.5}' +
+		'</style>' +
+		'<audio src="' + this.config.src + '"></audio></div>';
+	this.container = document.getElementById(this.config.id);
+	this.barPlay = document.querySelector('#' + this.config.id + ' .played');
+	this.barBuffer = document.querySelector('#' + this.config.id + ' .buffered');
+	this.audio = document.querySelector('#' + this.config.id + ' audio');
+	this.svgIcon = document.querySelector('#' + this.config.id + ' .icon');
+	var self = this;
+	this.updateTime = function (p) {
+		this.barPlay.style.width = p + '%';
+	};
+	var dragging = !1,
+		dragged = !1,
+		touchDevice = false;
+	this.listeners = {
+		timeupdate: function () {
+			if (!dragging)
+				self.updateTime(this.currentTime / this.duration * 100);
+			try {
+				var s, e, i = 0;
+				for (; i < this.buffered.length; i++) {
+					if (this.currentTime > this.buffered.start(i) && this.currentTime < this.buffered.end(i)) {
+						s = this.buffered.start(i);
+						e = this.buffered.end(i);
 						break;
 					}
 				}
-				if(i==this.buffered.length)
+				if (i == this.buffered.length)
 					throw 'Not Range Found';
-				self.barBuffer.style.width=(e/this.duration)*100+'%';
-			}catch(e){
+				self.barBuffer.style.width = (e / this.duration) * 100 + '%';
+			} catch (e) {
 				return;
 			}
 		},
-		progress:function(e){
-			try{
-				var s,e,i=0;
-				for(;i<this.buffered.length;i++){
-					if( this.currentTime>this.buffered.start(i) && this.currentTime<this.buffered.end(i) ){
-						s=this.buffered.start(i);
-						e=this.buffered.end(i);
+		progress: function () {
+			try {
+				var s, e, i = 0;
+				for (; i < this.buffered.length; i++) {
+					if (this.currentTime > this.buffered.start(i) && this.currentTime < this.buffered.end(i)) {
+						s = this.buffered.start(i);
+						e = this.buffered.end(i);
 						break;
 					}
 				}
-				if(i==this.buffered.length)
+				if (i == this.buffered.length)
 					throw 'Not Range Found';
-				self.barBuffer.style.width=(e/this.duration)*100+'%';
-			}catch(e){
+				self.barBuffer.style.width = (e / this.duration) * 100 + '%';
+			} catch (e) {
 				return;
 			}
 		},
-		click:function(e){
-			if(self.dragged){
-				self.dragged=!1;
-				return;
-			}
-			if(self.audio.paused){
+		click: function (e) {
+			console.log('click');
+			if (dragged) return;
+			if (self.audio.paused) {
 				self.audio.play();
-			}else{
+			} else {
 				self.audio.pause();
 			}
 		},
-		play:function(e){
-			self.svgIcon.innerHTML=self.resource.pause;
+		play: function (e) {
+			self.svgIcon.innerHTML = resource.pause;
 		},
-		pause:function(e){
-			self.svgIcon.innerHTML=self.resource.play;
+		pause: function (e) {
+			self.svgIcon.innerHTML = resource.play;
 		},
-		mousedown:function(e){
-			self.dragging=!0;
-			self.barPlay.style.transitionDuration='0s';
+		mousedown: function (e) {
+			console.log('mousedown');
+			if (touchDevice) return;
+			dragging = !0;
+			dragged = false;
+			self.barPlay.style.transitionDuration = '0s';
 		},
-		mousemove:function(e){
-			if(self.dragging){
-				self.dragged=!0;
-				var border=parseInt(self.config.borderWidth);
-				if(isNaN(border))
-					border=0;
-				var p=(e.layerX-border)/(self.container.offsetWidth-2*border)*100;
-				p=(p>100)?100:p;
+		mousemove: function (e) {
+			if (touchDevice) return;
+			if (dragging) {
+				console.log('mousemove');
+				dragged = !0;
+				var border = parseInt(self.config.borderWidth);
+				if (isNaN(border))
+					border = 0;
+				var p = (e.layerX - border) / (self.container.offsetWidth - 2 * border) * 100;
+				p = (p > 100) ? 100 : p < 0 ? 0 : p;
 				self.updateTime(p);
 			}
 		},
-		mouseup:function(e){
-			self.dragging=!1;
-			self.barPlay.style.transitionDuration='';
-			if(self.dragged){
-				self.audio.currentTime=parseFloat(self.barPlay.style.width)*self.audio.duration/100;
-				self.dragged=!1;
+		mouseup: function (e) {
+			if (touchDevice) return;
+			console.log('mouseup');
+			dragging = !1;
+			self.barPlay.style.transitionDuration = '';
+			if (dragged) {
+				e.preventDefault();
+				self.audio.currentTime = parseFloat(self.barPlay.style.width) * self.audio.duration / 100;
 			}
 		},
-		touchstart:function(e){
-			self.dragging=!0;
-			var bounding=self.barPlay.getBoundingClientRect();
-			self.oldTouch=[e.touches[0].clientX-bounding.left,e.touches[0].clientY-bounding.top];
-			self.barPlay.style.transitionDuration='0s';
+		touchstart: function (e) {
+			touchDevice = true;
+			dragging = !0;
+			dragged = false;
+			var bounding = self.barPlay.getBoundingClientRect();
+			self.oldTouch = [e.touches[0].clientX - bounding.left, e.touches[0].clientY - bounding.top];
+			self.barPlay.style.transitionDuration = '0s';
 		},
-		touchmove:function(e){
-			var bounding=self.barPlay.getBoundingClientRect(),
-			containerBounding=this.getBoundingClientRect(),
-			pos=[e.changedTouches[0].clientX-bounding.left,e.changedTouches[0].clientY-bounding.top];
+		touchmove: function (e) {
+			var bounding = self.barPlay.getBoundingClientRect(),
+				containerBounding = this.getBoundingClientRect(),
+				pos = [e.changedTouches[0].clientX - bounding.left, e.changedTouches[0].clientY - bounding.top];
 			//Vertical scrolling, ignore
-			if(pos[0]==self.oldTouch[0] && !self.dragged){
+			if (pos[0] == self.oldTouch[0] && !dragged) {
 				return;
 			}
 			//If touch moves out of container, stop tracking
-			if( pos[0]>=0 && pos[1] >=0 && pos[0] <= containerBounding.width && pos[1] <=containerBounding.height ){
-				self.barPlay.style.transitionDuration='0s';
-				self.dragging=!0;
-			}else{
-				self.barPlay.style.transitionDuration='';
-				self.dragging=!1;
+			if (pos[0] >= 0 && pos[1] >= 0 && pos[0] <= containerBounding.width && pos[1] <= containerBounding.height) {
+				self.barPlay.style.transitionDuration = '0s';
+				dragging = !0;
+			} else {
+				self.barPlay.style.transitionDuration = '';
+				dragging = !1;
 				return;
 			}
 			e.preventDefault();
-			self.dragged=!0;
-			var border=parseInt(self.config.borderWidth);
-			if(isNaN(border))
-				border=0;
-			var p=(pos[0])/(self.container.offsetWidth-2*border)*100;
-			p=(p>100)?100:((p<0)?0:p);
+			dragged = !0;
+			var border = parseInt(self.config.borderWidth);
+			if (isNaN(border))
+				border = 0;
+			var p = (pos[0]) / (self.container.offsetWidth - 2 * border) * 100;
+			p = (p > 100) ? 100 : ((p < 0) ? 0 : p);
 			self.updateTime(p);
 		},
-		touchend:function(e){
-			if(self.dragging && self.dragged){
-				self.audio.currentTime=parseFloat(self.barPlay.style.width)*self.audio.duration/100;
-				self.dragged=!1;
+		touchend: function (e) {
+			if (dragging && dragged) {
+				self.audio.currentTime = parseFloat(self.barPlay.style.width) * self.audio.duration / 100;
 			}
-			self.dragging=!1;
+			dragging = !1;
 			delete self.oldTouch;
-			self.barPlay.style.transitionDuration='';
+			self.barPlay.style.transitionDuration = '';
 		}
-	}
-	this.audio.addEventListener('timeupdate',this.listeners.timeupdate);
-	this.audio.addEventListener('progress',this.listeners.progress);
-	this.audio.addEventListener('play',this.listeners.play);
-	this.audio.addEventListener('pause',this.listeners.pause);
-	this.container.addEventListener('click',this.listeners.click);
-	this.container.addEventListener('mousedown',this.listeners.mousedown);
-	this.container.addEventListener('mousemove',this.listeners.mousemove);
-	this.container.addEventListener('mouseup',this.listeners.mouseup);
-	this.container.addEventListener('touchstart',this.listeners.touchstart);
-	this.container.addEventListener('touchmove',this.listeners.touchmove);
-	this.container.addEventListener('touchend',this.listeners.touchend);
-	if(this.config.autoPlay){
+	};
+	this.audio.addEventListener('timeupdate', this.listeners.timeupdate);
+	this.audio.addEventListener('progress', this.listeners.progress);
+	this.audio.addEventListener('play', this.listeners.play);
+	this.audio.addEventListener('pause', this.listeners.pause);
+	this.container.addEventListener('click', this.listeners.click);
+	this.container.addEventListener('mousedown', this.listeners.mousedown);
+	window.addEventListener('mousemove', this.listeners.mousemove);
+	window.addEventListener('mouseup', this.listeners.mouseup);
+	this.container.addEventListener('touchstart', this.listeners.touchstart);
+	this.container.addEventListener('touchmove', this.listeners.touchmove);
+	this.container.addEventListener('touchend', this.listeners.touchend);
+	if (this.config.autoPlay) {
 		this.listeners.click();
 	}
-	if(this.config.loop){
-		this.audio.loop=1;
+	if (this.config.loop) {
+		this.audio.loop = 1;
 	}
-}
-MusicPlayer.prototype.resource={
-	play:'<svg style="width:18px;height:18px"><polygon style="fill:#333;stroke:#FFF" points="2,2 2,16 16,9"></polygon></svg>',
-	pause:'<svg style="width:18px;height:18px"><polygon style="fill:#333;stroke:#FFF" points="2,2 2,16 8,16 8,2"></polygon><polygon style="fill:#333;stroke:#FFF" points="10,2 10,16 16,16 16,2"></polygon></svg>'
-}
+};
